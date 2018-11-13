0fbdeb1ee8f21548d3979b6bb2cb7abd923f8ad9
Ducktard <johannes_weyers@gmx.de>
2018-11-13 22:34:29 +0100

    proposal edit

--- 0 Data Aggregation/scrapingIntraDay.py
+++ 0 Data Aggregation/scrapingIntraDay.py
@@ -6,7 +6,20 @@ import numpy as np
 #Define the link
 url='https://www.epexspot.com/de/marktdaten/intradayauktion'
 # Define the dates for scraping data from
-datelist = pd.date_range(pd.datetime(2016,1,1), periods=731).tolist()
+startdate = pd.datetime(2016,1,1,0)
+datelist = pd.date_range(startdate, periods=731,freq='D').tolist()
+# Define the path where to save the scraped Data
+outputPath ='./00 Data/quarterhourlyIntraDayData.csv'
+#Define quarterhours in minutes
+quarterhours = [0,15,30,45] 
+
+#  SAVE LIST To CSV METHOD:
+def saveToCSV(startdate,hours,hourlyPrices,hourlyConsumption,path):
+    df  =pd.DataFrame(columns=['datetime','price','consumption'])
+    df['datetime'] = hours
+    df['price'] = hourlyPrices
+    df['consumption'] = hourlyConsumption
+    df.to_csv(path,index=False,sep=',')
 
 #Open URL and create RoboBrowser Instance
 browser = RoboBrowser(history=True)
@@ -14,6 +27,7 @@ browser.open(url)
 
 form = browser.get_forms('date_selector')
 
+hours = []
 hourlyPrices = []
 hourlyConsumption = []
 # df  =pd.DataFrame(columns=['datetime','price','consumption'])
@@ -23,37 +37,33 @@ for date in datelist:
     #Submit form
     browser.submit_form(form[0])
 
-    #pass code to beatufulsoup tool
-    html = bs(str(browser.parsed), 'html.parser')
-  
-    hourlyTable = html.find('table',attrs={'class':'list hours responsive'})
-    tableBody = hourlyTable.find('tbody')
-    rows = tableBody.find_all('tr')
-    count = 0
-    # print(date)
-    for row in rows:
-        columns = row.find_all('td')
-        if columns:
-            if count % 2 == 0:
-                hourlyPrices.append(columns[8].text)
-            else:
-                hourlyConsumption.append(columns[8].text)
-            count += 1
-            # print(columns[8].value)
     
-    iteration = date - pd.datetime(2016,1,1)
-    iteration = (iteration / np.timedelta64(1, 'D')).astype(int) +1
-    print(iteration)
-    if iteration % 30 == 0 or iteration == 731:
-        df  =pd.DataFrame(columns=['datetime','price','consumption'])
-        df['datetime'] = pd.date_range(pd.datetime(2016,1,1,0), periods=(24*iteration),freq='H').tolist()
-        df['price'] = hourlyPrices
-        df['consumption'] = hourlyConsumption
-        print(df)
-        df.to_csv('./00 Data/hourlyDayAheadData.csv',index=False,sep=',')
+    for h in range(0,23,1):
+        link = browser.find('a',attrs={'id':'val_01'})[0]
+        browser.follow_link(link)
+        #pass code to beatufulsoup tool
+        html = bs(str(browser.parsed), 'html.parser')
+        hourlyTable = html.find('table',attrs={'class':'list hours responsive'})
+        for m in quarterhours:
+            tableBody = hourlyTable.find('tbody')
+            rows = tableBody.find_all('tr')
+            count = 0
+            for row in rows:
+                columns = row.find_all('td')
+                if columns:
+                    if count % 2 == 0:
+                        s = int(columns[0].text.strip()[-1:])
+                        hours.append(pd.datetime(date.year,date.month,date.day,h,s))
+                        hourlyPrices.append(columns[9].text)
+                    else:
+                        hourlyConsumption.append(columns[9].text)
+                        
+                    count += 1
+            
+            iteration = date - startdate
+            iteration = iteration.days
+            print("Iteration: " + str(iteration) + " scraped Date: " + str(date))
+            if iteration % 30 == 0:
+                saveToCSV(startdate,hours,hourlyPrices,hourlyConsumption,outputPath)
 
-# datelist = pd.date_range(pd.datetime(2016,1,1,0), periods=(731*24),freq='H').tolist()
-# df['price'] = hourlyPrices
-# df['consumption'] = hourlyConsumption                
-
-# df.to_csv('./Data/hourlyDayAheadData.csv',index=False)
+saveToCSV(startdate,iteration,hourlyPrices,hourlyConsumption,outputPath)
