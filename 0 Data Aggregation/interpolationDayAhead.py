b687701e4679e72817f9b1226b307db81b60cf29
Rufin Korbmacher <rkorbma1@smail.uni-koeln.de>
2018-11-15 13:16:54 +0100

    Ok ist jetzt ein bisschen schöner

--- 0 Data Aggregation/interpolationDayAhead.py
+++ 0 Data Aggregation/interpolationDayAhead.py
@@ -5,29 +5,23 @@ import datetime
 
 path ='../00 Data/hourlyDayAheadData.csv'
 df= pd.read_csv(path)
-dataframe_0=pd.read_csv(path)
+
 
 #Convert the datetime column of the dataframe from strings to timestamps
-def convertion(dataframe):
-    date = pd.to_datetime(dataframe["datetime"])
-    new_date= add_15_minutes(date)
-    return new_date
-
 #Add to every timestamp 15 minutes
-def add_15_minutes(date):
+def add_15_minutes(dataframe, x):
+    dbf= pd.to_datetime(dataframe["datetime"])
     new_date = []
-    for i in date:
-        new_datetime = i + datetime.timedelta(minutes=15)
+    for i in dbf:
+        new_datetime = i + datetime.timedelta(minutes=x)
         new_date.append(new_datetime)
     return new_date
 
 #Create a new dataframe, which can be appended to the old one
 def create_new_dataframe(date):
-    new_dataframe = pd.DataFrame(df)
-    index = df.columns.get_loc("datetime")
-    valueOfColumn = df.iloc[0:, index]
+    new_dataframe= pd.read_csv(path)
     del new_dataframe["datetime"]
-    new_dataframe.insert(index, "datetime",date)
+    new_dataframe["datetime"]=date
     return new_dataframe
 
 #Sort the whole datastream with a timestamp
@@ -37,22 +31,24 @@ def sort(dataframe):
 
 
 #First dataframe with 15 minutes
-date_15= convertion(df)
+date_15= add_15_minutes(df,15)
+#print(date_15)
 dataframe_15=create_new_dataframe(date_15)
-dataframe_1= dataframe_0.append([dataframe_15], ignore_index=True)
 
 #second dataframe with 30 minutes
-date_30= convertion(dataframe_15)
+date_30= add_15_minutes(df,30)
+#print(date_30)
 dataframe_30= create_new_dataframe(date_30)
-dataframe_2= dataframe_1.append(dataframe_30, ignore_index=True)
 
 #third dataframe with 45 minutes
-date_45= convertion(dataframe_30)
+date_45= add_15_minutes(df,45)
+#print(date_45)
 dataframe_45= create_new_dataframe(date_45)
-
-interpolated_dataframe= dataframe_2.append(dataframe_45, ignore_index=True)
+interpolated_dataframe= pd.concat([df,dataframe_15,dataframe_30,dataframe_45], ignore_index=True, sort=True)
 
 sorted_interpolated_dataframe= sort(interpolated_dataframe)
+print(sorted_interpolated_dataframe)
+
 
 #Save the datastream to a new .csv file
 save_path= '../00 Data/interpolatedDayAhead.csv'
