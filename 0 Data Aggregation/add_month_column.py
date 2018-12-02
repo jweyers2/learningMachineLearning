e616e2ed724dd4834958c9212b3b59a5da005ebf
Jul <buggitheclown@web.de>
2018-12-02 15:06:33 +0100

    Month column reworked

--- 0 Data Aggregation/add_month_column.py
+++ 0 Data Aggregation/add_month_column.py
@@ -1,10 +1,20 @@
 import pandas as pd
-import numpy as np
 
 path='../00 Data/Final/cleanFinal.csv'
 dataframe=pd.read_csv(path)
 
-dataframe['month'] = pd.DatetimeIndex(dataframe['datetime']).month
+dataframe['january'] = (pd.DatetimeIndex(dataframe['datetime']).month == 1).astype(int)
+dataframe['february'] = (pd.DatetimeIndex(dataframe['datetime']).month == 2).astype(int)
+dataframe['march'] = (pd.DatetimeIndex(dataframe['datetime']).month == 3).astype(int)
+dataframe['april'] = (pd.DatetimeIndex(dataframe['datetime']).month == 4).astype(int)
+dataframe['may'] = (pd.DatetimeIndex(dataframe['datetime']).month == 5).astype(int)
+dataframe['june'] = (pd.DatetimeIndex(dataframe['datetime']).month == 6).astype(int)
+dataframe['july'] = (pd.DatetimeIndex(dataframe['datetime']).month == 7).astype(int)
+dataframe['august'] = (pd.DatetimeIndex(dataframe['datetime']).month == 8).astype(int)
+dataframe['september'] = (pd.DatetimeIndex(dataframe['datetime']).month == 9).astype(int)
+dataframe['october'] = (pd.DatetimeIndex(dataframe['datetime']).month == 10).astype(int)
+dataframe['november'] = (pd.DatetimeIndex(dataframe['datetime']).month == 11).astype(int)
+dataframe['december'] = (pd.DatetimeIndex(dataframe['datetime']).month == 12).astype(int)
 
-dataframe.to_csv(path,index=False)
+dataframe.to_csv(path, index=False)
 
