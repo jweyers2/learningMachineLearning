fb00be6ef3f7f408701d2f568dbf3d9e7161bccf
Jul <buggitheclown@web.de>
2018-12-02 20:50:33 +0100

    Examined the data to decide on the measurement for classifiers

---
+++ 1 Data Understanding/classification_measurement_research.py
@@ -0,0 +1,11 @@
+# Check the percentage of rows that have the value 1 in the column "cat_price_premium"
+# If there isn't a strong imbalance use ROC score, otherwise F1 score
+import pandas as pd
+
+path = '../00 Data/Final/final.csv'
+dataframe = pd.read_csv(path, dayfirst=True)
+totalAmount = len(dataframe.index)
+dfPositive = dataframe.loc[dataframe['cat_price_premium'] == 1]
+positivePremiumAmount = len(dfPositive.index)
+print (positivePremiumAmount/totalAmount)
+# Result: 70% positive premium
