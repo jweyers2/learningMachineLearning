676a741c91e859bcecd19642c9470ae86a7e6a01
Jul <buggitheclown@web.de>
2018-12-04 13:27:23 +0100

    Cross validation framework

--- 3 Data Modeling/Predictive/Binary_Classifier/model.py
+++ 3 Data Modeling/Predictive/Binary_Classifier/model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import TimeSeriesSplit
 import sys
 sys.path.insert(0, '../../')
 from utils import train_test_split_predictive
@@ -11,5 +12,10 @@ if (useDevDataset):
 else:
     path = '../../../00 Data/Final/scaledFinal.csv'
 X_train, X_test, y_train, y_test = train_test_split_predictive(useDevDataset, path)
+tscv = TimeSeriesSplit(n_splits=5)
+for train_index, test_index in tscv.split(X_train):
+    print("TRAIN:", train_index, "\n", "TEST:", test_index)
+    X_train_train, X_train_test = X_train[:train_index.size], X_train[train_index.size:train_index.size+test_index.size]
+    y_train_train, y_train_test = y_train[:train_index.size], y_train[train_index.size:train_index.size+test_index.size]
 print(1)
 
