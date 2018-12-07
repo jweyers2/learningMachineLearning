61d462153a8c173bfa31814f5813edfdaa42cb82
Jul <buggitheclown@web.de>
2018-12-07 16:26:51 +0100

    Removed cat price premium from descriptive set

--- 0 Data Aggregation/add_categorical_forwardpremium.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-path = '../00 Data/Final/final.csv'
-dataframe = pd.read_csv(path, dayfirst=True)
-dataframe['cat_price_premium'] = (dataframe['price_premium'] > 0).astype(int)
-# = 1 if day ahead price > intraday price
-
-dataframe.to_csv(path, index=False)
-
