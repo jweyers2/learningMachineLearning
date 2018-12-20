18e3630a1408766c90a33ad762867a94c86ef963
Ducktard <johannes_weyers@gmx.de>
2018-12-20 16:27:53 +0100

    almost finished arima, (reasoning of pattern is still left)

--- 3 Data Modeling/descriptive/arima.py
+++ 3 Data Modeling/descriptive/arima.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import arima_utils as utils
+import matplotlib.pyplot as plt
+
 path = '../../00 Data/Final/cleanFinal.csv'
 
 df = pd.read_csv(path,sep=',')
@@ -8,11 +10,20 @@ df.set_index('datetime', inplace=True)
 
 
 
-def ARIMA(df):
-    utils.stationaryTest(df)
 
 def main():
-    ARIMA(df)
+    # utils.stationaryTest(df)
+    # utils.printPACFACF(df['price_premium'])
+    predictions,results = utils.arima(df['price_premium'], df[
+        ['dailyTempAvg(Celsius)', 'numberIcyDays', 'numberFreezingDays', 'monthlyRainVolume(mm)', 'numberRainyDays',
+         'dailySunnyHoursAvg', 'monthlyWindSpeedAvg(km/h)', 'holidayBefore', 'holidayAfter', 'isHoliday',
+         'participants']])
+    # predictions,results = utils.arima(df['price_premium'])
+    print(results.summary())
+    print(results.maparams())
+    plt.plot(df['price_premium'], color='blue')
+    plt.plot(predictions, color='red')
+    plt.show()
 
 if __name__ == '__main__':
     main()
--- 3 Data Modeling/descriptive/arima_utils.py
+++ 3 Data Modeling/descriptive/arima_utils.py
@@ -2,26 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-path = '../../00 Data/Final/cleanFinal.csv'
-
-df = pd.read_csv(path,sep=',')
-df['datetime']= pd.to_datetime(df['datetime'])
-df.set_index('datetime', inplace=True)
 
 """
-First Run at 1% Level: -3,4 --> stationary : IS THAT CORRECT?? -> validate!!!
-sources by https://machinelearningmastery.com/time-series-data-stationary-python/
+TIMESERIES IS STATIONARY
+sources:
+https://machinelearningmastery.com/time-series-data-stationary-python/
 https://machinelearningmastery.com/time-series-forecast-case-study-python-monthly-armed-robberies-boston/
 """
-def stationaryTest(df):
+def stationaryTest(ts):
     from statsmodels.tsa.stattools import adfuller
 
     # Determing rolling statistics
-    rolmean = df['price_premium'].rolling(12).mean()
-    rolstd = df['price_premium'].rolling(12).std()
+    rolmean = ts.rolling(12).mean()
+    rolstd = ts.rolling(12).std()
 
     # Plot rolling statistics:
-    orig = plt.plot(df['price_premium'], color='blue', label='Original')
+    orig = plt.plot(ts, color='blue', label='Original')
     mean = plt.plot(rolmean, color='red', label='Rolling Mean')
     std = plt.plot(rolstd, color='black', label='Rolling Std')
     plt.legend(loc='best')
@@ -29,7 +25,7 @@ def stationaryTest(df):
     # plt.show(block=False)
     plt.show()
 
-    result = adfuller(df['price_premium'].values)
+    result = adfuller(ts.values)
     print('ADF Statistic: %f' % result[0])
     print('p-value: %f' % result[1])
     print('Critical Values:')
@@ -38,32 +34,31 @@ def stationaryTest(df):
 
     return result
 
-# stationaryTest(df)
 
 """
 PACF / ACF
 RIGHT VALUES FOR P <--ACF / Q <--PACF
 HOW TO DETERMINE THE ACF THRESHOLDS:
 https://stats.stackexchange.com/questions/185425/how-to-determine-the-critical-values-of-acf
+http://sfb649.wiwi.hu-berlin.de/fedc_homepage/xplore/tutorials/xegbohtmlnode39.html
+https://sccn.ucsd.edu/wiki/Chapter_3.6._Model_Validation
 """
 
-def printPACFACF(df):
-    def cube(x):
-        if 0 <= x: return x ** (1. / 3.)
-        return -(-x) ** (1. / 3.)
+def printPACFACF(price_premium):
     from statsmodels.tsa.stattools import acf, pacf
-    price_premium = df['price_premium']
-    # print(price_premium)
-    pp_log = price_premium
-    lag_acf = acf(pp_log, nlags=6000)
+    ACF_PLOT_RANGE = 6000
+    pp_log = price_premium.values
+    lag_acf = acf(pp_log, nlags=ACF_PLOT_RANGE)
     lag_pacf = pacf(pp_log, nlags=20, method='ols')
     # Plot ACF:
     plt.subplot(121)
     plt.plot(lag_acf)
     plt.axhline(y=0, linestyle='--', color='gray')
-    print(np.sqrt((len(pp_log))))
-    plt.axhline(y=-1.96 / np.sqrt(len(pp_log)), linestyle='--', color='gray')
-    plt.axhline(y=1.96 / np.sqrt(len(pp_log)), linestyle='--', color='gray')
+    x = pd.Series(range(ACF_PLOT_RANGE))
+    y_ = (-1.96 / np.sqrt(len(pp_log)-x))
+    y = (1.96 / np.sqrt(len(pp_log)-x))
+    plt.plot(x.values, y_.values, linestyle='--', color='gray')
+    plt.plot(x.values,y.values, linestyle='--', color='gray')
     plt.title('Autocorrelation Function')
 
     # Plot PACF:
@@ -74,30 +69,86 @@ def printPACFACF(df):
     plt.axhline(y=1.96 / np.sqrt(len(pp_log)), linestyle='--', color='gray')
     plt.title('Partial Autocorrelation Function')
     plt.tight_layout()
-    # plt.show()
+    plt.show()
 
 
     from statsmodels.graphics.tsaplots import plot_acf
     plot_acf(pp_log)
     plt.show()
 
-printPACFACF(df)
+
+"""
+Decomposition
+"""
+def decompose(timeseries):
+    from statsmodels.tsa.seasonal import seasonal_decompose
+    decomposition_ts_saisonal_1 = seasonal_decompose(timeseries, freq=96)
+    # decomposition_ts_saisonal_2 = seasonal_decompose(timeseries, freq=672)
+
+    # Seasonal
+    seasonal_1 = decomposition_ts_saisonal_1.seasonal
+    # seasonal_2 = decomposition_ts_saisonal_2.seasonal
+    seasonal_1.fillna(inplace=True, value=seasonal_1.mean())
+    # seasonal_2.fillna(inplace=True, value=seasonal_2.mean())
+    detrend_ts = decomposition_ts_saisonal_1.resid
+    # detrend_ts = detrend_ts - seasonal_2
+
+    # Subtract the seasonal to make TS stationary
+    # detrend_ts = timeseries - seasonal
+    detrend_ts.fillna(value=detrend_ts.mean(), inplace=True)
+
+    # Plot
+    fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+    axs[0].plot(timeseries, label='Original')
+    plt.legend(loc='best')
+    axs[1].plot(seasonal_1,label='Seasonality')
+    plt.legend(loc='best')
+    axs[2].plot(detrend_ts, label='residual')
+    plt.legend(loc='best')
+    fig.autofmt_xdate()
+
+    plt.show()
+
+    # return (detrend_ts, seasonal_1,seasonal_2)
+    return (detrend_ts, seasonal_1)
 
 """
 TODO: PACF / ACF --> STRUCTURED DETERMINATION OF P , Q (D IS = 0 )
 sources partially by: https://www.analyticsvidhya.com/blog/2016/02/time-series-forecasting-codes-python/
 ASSUMPTION FOR FURTHER MODELING -> TIMESERIES STATIONARY (DICKEY-FULLER-TEST)
 """
-def arima(df):
+def arima(ts,exVar=None):
     from statsmodels.tsa.arima_model import ARIMA
-    import datetime
-    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-    model = ARIMA(df['price_premium'].values,order=(2, 1, 0))
-    results_AR = model.fit(disp=-1)
-    # plt.plot(df['price_premium'].values,color='b')
+    TRAIN_RATIO = 0.7
+    date_index = ts.index
+    from sklearn.metrics import mean_absolute_error
+    from sklearn.model_selection import TimeSeriesSplit
+    tscv = TimeSeriesSplit(n_splits=2)
+    errors = []
+    for train, test in tscv.split(ts.values):
+        p = 1
+        d = 0
+        q = 1
+        if exVar is None:
+            model = ARIMA(ts.iloc[train], dates=np.asanyarray(ts.index[train]), order=(p, d, q))
+        else:
+            model = ARIMA(ts.iloc[train], dates=np.asanyarray(ts.index[train]), exog=exVar.iloc[train, :],
+                          order=(p, d, q))
+        results_AR = model.fit(disp=-1)
+        if exVar is None:
+            predictions = results_AR.predict(start=ts.index[test[0]].to_pydatetime(),end=ts.index[test[-1]].to_pydatetime(), dynamic=True)
+            # predictions = results_AR.forecast(steps=1000)
+        else:
+           predictions = results_AR.predict(start=ts.index[test[0]].to_pydatetime(),end=ts.index[test[-1]].to_pydatetime(),exog=exVar.iloc[train,:],dynamic=True)
 
-    ax1.plot(results_AR.fittedvalues, color='red')
-    ax2.plot(results_AR.predict(start = datetime.datetime(year=2016, month=1, day = 1, hour = 0,minute=0), end = datetime.datetime(year=2017, month=12, day = 31, hour = 23,minute=45), dynamic=True), color='green')
-    plt.show()
+        errors.append(mean_absolute_error(ts.iloc[test],predictions))
 
-# arima(df)
+    print("MAE:" + str(np.mean(errors)))
+    predictions = pd.Series(predictions)
+    import datetime as dt
+    # predictions.index = pd.date_range(dt.datetime(year=2018,month=1,day=1,hour=0,minute=0), periods=1000).tolist()
+    predictions.index = date_index[test]
+    return predictions,results_AR
+
+
+
