24eaa8effbf3388c0eb0a3c1ef0ea7adaf185379
cangaroo6 <kimferres@gmail.com>
2019-01-16 13:53:43 +0100

    Values added to model comparison
    values still need to be sorted

--- 3 Data Modeling/Predictive/modelComparison.py
+++ 3 Data Modeling/Predictive/modelComparison.py
@@ -1,13 +1,29 @@
 # libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # set width of bar
 barWidth = 0.25
 
-#values
-accuracy = [0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8]
-roc = [0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8]
+accuracy = [0.79,0.8,0.66,0.77,0.8,0.8,0.814,0.0,0.72]
+roc =      [0.72,0.73,0.7,0.74,0.73,0.72,0.75,0.0,0.5]
+
+
+# values = [accuracy, roc]
+# df = pd.DataFrame(values, columns=['accuracy', 'roc'],
+#     index=['SGD','SVC','GaussianNB', 'Soft Voting', 'Hard Voting', 'Logistic Reg.', 'RNN', 'Decision Tree', 'Random Forest'])
+# df.loc[len(df)] = [0.79, 0.72] #SGD
+# df.loc[len(df)] = [0.8, 0.73] #SVC
+# df.loc[len(df)] = [0.66, 0.7] #GNB
+# df.loc[len(df)] = [0.77, 0.74] #Soft
+# df.loc[len(df)] = [0.8, 0.73] #Hard
+# df.loc[len(df)] = [0.8, 0.72] #Logistic
+# df.loc[len(df)] = [0.814, 0.75] #RNN
+# df.loc[len(df)] = [0, 0] #Tree
+# df.loc[len(df)] = [0.72, 0.5] #RF
+# df.sort_values(by='accuracy', ascending=False)
+# print(df)
 
 # Set position of bar on X axis
 r1 = np.arange(len(accuracy))
@@ -20,8 +36,8 @@ plt.bar(r2, roc, color='#81F781', width=barWidth, edgecolor='white', label='roc'
 
 # Add xticks on the middle of the group bars
 plt.xlabel('group', fontweight='bold')
-plt.xticks([r + barWidth for r in range(len(accuracy))], ['SGD','SVC','GaussianNB', 'Soft Voting', 'Hard Voting', 'Logistic Reg.', 'RNN', 'Decision Tree', 'ARIMA', 'Random Forest'])
-plt.xticks(rotation=70)
+plt.xticks([r + barWidth for r in range(len(accuracy))], ['SGD','SVC','Gaussian NB', 'Soft Voting', 'Hard Voting', 'Logistic Reg.', 'RNN', 'Decision Tree', 'Random Forest'])
+plt.xticks(rotation=60)
 
 #Labels
 plt.xlabel('Model')
@@ -36,6 +52,7 @@ plt.gcf().subplots_adjust(bottom=0.3)
 axes = plt.gca()
 axes.set_ylim([0, 1])
 
-# Create legend & Show graphic
-#plt.show()
+#set y axis
+plt.ylim([0.4, 1])
+
 plt.savefig('./modelComparision.png')
